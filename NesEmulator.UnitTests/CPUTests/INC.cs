e3f8262d214c512f4d7134474de2e11aa6f2b280
Patrick Allwood <patchandthat@gmail.com>
2019-04-04 12:59:49 +0100

    Added some failing tests

---
+++ NesEmulator.UnitTests/CPUTests/INC.cs
@@ -0,0 +1,140 @@
+using FakeItEasy;
+using NesEmulator.Processor;
+using Xunit;
+
+namespace NesEmulator.UnitTests.CPUTests
+{
+    public partial class CPUTests
+    {
+        public class INC
+        {
+            public class ZeroPage
+            {
+                private IMemory _memory;
+                private OpCode _op;
+
+                public ZeroPage()
+                {
+                    _memory = A.Fake<IMemory>();
+                    _op = new OpcodeDefinitions().FindOpcode(Operation.INC, AddressMode.ZeroPage);
+
+                    A.CallTo(() => _memory.Read(MemoryMap.ResetVector))
+                        .Returns((byte) 0x00);
+                    A.CallTo(() => _memory.Read(MemoryMap.ResetVector + 1))
+                        .Returns((byte) 0x80);
+                }
+
+                private CPU CreateSut()
+                {
+                    var cpu = new CPU(_memory);
+                    cpu.Power();
+                    cpu.Step();
+                    Fake.ClearRecordedCalls(_memory);
+                    return cpu;
+                }
+                
+                [Fact]
+                public void Todo()
+                {
+                    Assert.True(false, "Todo: ");
+                }
+            }
+
+            public class ZeroPageX
+            {
+                private IMemory _memory;
+                private OpCode _op;
+
+                public ZeroPageX()
+                {
+                    _memory = A.Fake<IMemory>();
+                    _op = new OpcodeDefinitions().FindOpcode(Operation.INC, AddressMode.ZeroPageX);
+
+                    A.CallTo(() => _memory.Read(MemoryMap.ResetVector))
+                        .Returns((byte) 0x00);
+                    A.CallTo(() => _memory.Read(MemoryMap.ResetVector + 1))
+                        .Returns((byte) 0x80);
+                }
+
+                private CPU CreateSut()
+                {
+                    var cpu = new CPU(_memory);
+                    cpu.Power();
+                    cpu.Step();
+                    Fake.ClearRecordedCalls(_memory);
+                    return cpu;
+                }
+                
+                [Fact]
+                public void Todo()
+                {
+                    Assert.True(false, "Todo: ");
+                }
+            }
+
+            public class Absolute
+            {
+                private IMemory _memory;
+                private OpCode _op;
+
+                public Absolute()
+                {
+                    _memory = A.Fake<IMemory>();
+                    _op = new OpcodeDefinitions().FindOpcode(Operation.INC, AddressMode.Absolute);
+
+                    A.CallTo(() => _memory.Read(MemoryMap.ResetVector))
+                        .Returns((byte) 0x00);
+                    A.CallTo(() => _memory.Read(MemoryMap.ResetVector + 1))
+                        .Returns((byte) 0x80);
+                }
+
+                private CPU CreateSut()
+                {
+                    var cpu = new CPU(_memory);
+                    cpu.Power();
+                    cpu.Step();
+                    Fake.ClearRecordedCalls(_memory);
+                    return cpu;
+                }
+                
+                [Fact]
+                public void Todo()
+                {
+                    Assert.True(false, "Todo: ");
+                }
+            }
+
+            public class AbsoluteX
+            {
+                private IMemory _memory;
+                private OpCode _op;
+
+                public AbsoluteX()
+                {
+                    _memory = A.Fake<IMemory>();
+                    _op = new OpcodeDefinitions().FindOpcode(Operation.INC, AddressMode.AbsoluteX);
+
+                    A.CallTo(() => _memory.Read(MemoryMap.ResetVector))
+                        .Returns((byte) 0x00);
+                    A.CallTo(() => _memory.Read(MemoryMap.ResetVector + 1))
+                        .Returns((byte) 0x80);
+                }
+
+                private CPU CreateSut()
+                {
+                    var cpu = new CPU(_memory);
+                    cpu.Power();
+                    cpu.Step();
+                    Fake.ClearRecordedCalls(_memory);
+                    return cpu;
+                }
+
+                [Fact]
+                public void Todo()
+                {
+                    Assert.True(false, "Todo: ");
+                }
+            }
+        }
+    }
+}
---
+++ NesEmulator.UnitTests/CPUTests/INX.cs
@@ -0,0 +1,120 @@
+using FakeItEasy;
+using FluentAssertions;
+using NesEmulator.Extensions;
+using NesEmulator.Processor;
+using NesEmulator.UnitTests.Helpers;
+using Xunit;
+
+namespace NesEmulator.UnitTests.CPUTests
+{
+    public partial class CPUTests
+    {
+        public class INX
+        {
+            public class Implicit
+            {
+                private IMemory _memory;
+                private OpCode _op;
+
+                public Implicit()
+                {
+                    _memory = A.Fake<IMemory>();
+                    _op = new OpcodeDefinitions().FindOpcode(Operation.INX, AddressMode.Implicit);
+
+                    A.CallTo(() => _memory.Read(MemoryMap.ResetVector))
+                        .Returns((byte) 0x00);
+                    A.CallTo(() => _memory.Read(MemoryMap.ResetVector + 1))
+                        .Returns((byte) 0x80);
+                }
+
+                private CPU CreateSut()
+                {
+                    var cpu = new CPU(_memory);
+                    cpu.Power();
+                    cpu.Step();
+                    Fake.ClearRecordedCalls(_memory);
+                    return cpu;
+                }
+
+                [Theory]
+                [InlineData(0x00, 0x01)]
+                [InlineData(0x48, 0x49)]
+                [InlineData(0xFF, 0x00)]
+                public void IncrementsXRegisterValue(byte start, byte expectedResult)
+                {
+                    var sut = CreateSut();
+                    sut.LDX(start, _memory);
+
+                    A.CallTo(() => _memory.Read(sut.InstructionPointer))
+                        .Returns(_op.Hex);
+                    
+                    sut.Step();
+
+                    sut.IndexX.Should().Be(expectedResult);
+                }
+
+                [Theory]
+                [InlineData(StatusFlags.None)]
+                [InlineData(StatusFlags.All)]
+                public void SetZeroFlagIfNewRegisterValueIsZero(StatusFlags initialFlags)
+                {
+                    Assert.True(false, "Todo: ");
+                }
+
+                [Theory]
+                [InlineData(StatusFlags.None)]
+                [InlineData(StatusFlags.All)]
+                public void ClearZeroFlagIfNewRegisterValueIsNotZero(StatusFlags initialFlags)
+                {
+                    Assert.True(false, "Todo: ");
+                }
+
+                [Theory]
+                [InlineData(StatusFlags.None)]
+                [InlineData(StatusFlags.All)]
+                public void SetNegativeFlagIfNewRegisterValueIsNegative(StatusFlags initialFlags)
+                {
+                    Assert.True(false, "Todo: ");
+                }
+
+                [Theory]
+                [InlineData(StatusFlags.None)]
+                [InlineData(StatusFlags.All)]
+                public void ClearNegativeFlagIfNewRegisterValueIsNotNegative(StatusFlags initialFlags)
+                {
+                    Assert.True(false, "Todo: ");
+                }
+
+                [Fact]
+                public void ExecutionTakes2Cycles()
+                {
+                    var sut = CreateSut();
+                    
+                    A.CallTo(() => _memory.Read(sut.InstructionPointer))
+                        .Returns(_op.Hex);
+
+                    var expectedCycles = sut.ElapsedCycles + 2;
+                    
+                    sut.Step();
+
+                    sut.ElapsedCycles.Should().Be(expectedCycles);
+                }
+
+                [Fact]
+                public void IncrementsInstructionPointerBy1()
+                {
+                    var sut = CreateSut();
+                    
+                    A.CallTo(() => _memory.Read(sut.InstructionPointer))
+                        .Returns(_op.Hex);
+
+                    var expectedPointer = sut.InstructionPointer.Plus(1);
+                    
+                    sut.Step();
+
+                    sut.InstructionPointer.Should().Be(expectedPointer);
+                }
+            }
+        }
+    }
+}
