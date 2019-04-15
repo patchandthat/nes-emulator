1f733609216974d9c04acaa1bd1e64970fb3714e
Patrick Allwood <patchandthat@gmail.com>
2019-04-15 13:10:05 +0100

    Tests

--- NesEmulator.UnitTests/CPUTests/OpcodeImplementations/CMP.cs
+++ NesEmulator.UnitTests/CPUTests/OpcodeImplementations/CMP.cs
@@ -1,5 +1,8 @@
 using FakeItEasy;
+using FluentAssertions;
+using NesEmulator.Extensions;
 using NesEmulator.Processor;
+using NesEmulator.UnitTests.Helpers;
 using Xunit;
 
 namespace NesEmulator.UnitTests.CPUTests.OpcodeImplementations
@@ -31,10 +34,77 @@ namespace NesEmulator.UnitTests.CPUTests.OpcodeImplementations
                 return cpu;
             }
 
-            [Fact]
-            public void Todo()
+            [Theory]
+            [InlineData(0x00, 0x01)]
+            [InlineData(0xFF, 0xFF)]
+            [InlineData(0x0F, 0x7F)]
+            public void SetsCarryFlagWhenTargetRegisterIsGreaterThanOrEqualToComparedValue(byte registerValue, byte comparisonValue)
             {
-                Assert.True(false, "Todo");
+                var sut = CreateSut();
+                sut.LDA(registerValue, _memory);
+                sut.ForceStatus(StatusFlags.None);
+
+                A.CallTo(() => _memory.Read(sut.InstructionPointer))
+                    .Returns(_op.Value);
+                A.CallTo(() => _memory.Read(sut.InstructionPointer.Plus(1)))
+                    .Returns(comparisonValue);
+                
+                sut.Step();
+
+                sut.Status.HasFlag(StatusFlags.Carry)
+                    .Should().BeTrue();
+            }
+
+            [Theory]
+            [InlineData(StatusFlags.None)]
+            [InlineData(StatusFlags.All)]
+            public void ClearsCarryFlagWhenTargetRegisterIsLessThanComparedValue(StatusFlags initialFlags)
+            {
+                Assert.True(false, "Todo: ");
+            }
+
+            [Theory]
+            [InlineData(StatusFlags.None)]
+            [InlineData(StatusFlags.All)]
+            public void SetsZeroFlagWhenTargetRegisterIsEqualToComparedValue(StatusFlags initialFlags)
+            {
+                Assert.True(false, "Todo: ");
+            }
+
+            [Theory]
+            [InlineData(StatusFlags.None)]
+            [InlineData(StatusFlags.All)]
+            public void ClearsZeroFlagWhenTargetRegisterIsNotEqualToComparedValue(StatusFlags initialFlags)
+            {
+                Assert.True(false, "Todo: ");
+            }
+
+            [Theory]
+            [InlineData(StatusFlags.None)]
+            [InlineData(StatusFlags.All)]
+            public void SetsNegativeFlagIfComparisonResultIsNegative(StatusFlags initialFlags)
+            {
+                Assert.True(false, "Todo: ");
+            }
+
+            [Theory]
+            [InlineData(StatusFlags.None)]
+            [InlineData(StatusFlags.All)]
+            public void ClearsNegativeFlagIfComparisonResultIsNotNegative(StatusFlags initialFlags)
+            {
+                Assert.True(false, "Todo: ");
+            }
+
+            [Fact]
+            public void ExecutionTakes2Cycles()
+            {
+                Assert.True(false, "Todo: + cycle penalty");
+            }
+
+            [Fact]
+            public void InstructionPointerIsIncremented()
+            {
+                Assert.True(false, "Todo: ");
             }
         }
         
