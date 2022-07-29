84823ffab5070ca870615ef2ceac3350bdb5c4db
Patrick <patchandthat@gmail.com>
2022-07-29 22:19:12 +0100

    Client window

---
+++ NesEmulator.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Raylib_cs;
+
+namespace NesEmulator.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(800, 600, "NES");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.RAYWHITE);
+
+                Raylib.DrawText("Hello C# Window", 10, 10, 20, Color.RED);
+
+                Raylib.EndDrawing();
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
